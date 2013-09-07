a3d42e5e3f6dc4652f2e81bba9113162ddee6e9c
Ryan Ruggero <rruggero@gmail.com>
2013-09-07 00:43:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirsApp::Application.config.secret_key_base = 'a812fb90d142063a1eba2a46e19bfbcd0f705ce22245b236b62bb31378024007ad3b99591144347fec81b6ddebf08463be05ecd25cb437943b0e36afd18321d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirsApp::Application.config.session_store :cookie_store, key: '_firs_app_session'
